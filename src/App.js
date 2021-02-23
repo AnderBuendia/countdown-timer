4de4703612de7385941c8725efd6ccad49d4d8b2
Ander <ibysytho@gmail.com>
2021-02-23 17:43:13 +0100

    firsts steps

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import './index.css';
 
 function App() {
   return (
-    <div>
-      <div className="main">
-        <h1>We're launching soon</h1>
+    <div className="container">
+
+        <h1 className="title">We're launching soon</h1>
 
         Days
         Hours
         Minutes
         Seconds
-      </div>
+    
+          
+
       <div className="footer">
-        Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
-        Coded by <a href="https://github.com/AnderBuendia">anderb</a>.
+        <div className="attribution">
+          Challenge by <a href="https://www.frontendmentor.io?ref=challenge">Frontend Mentor</a>. 
+          Coded by <a href="https://github.com/AnderBuendia">anderb</a>.
+        </div>
       </div>
     </div>
   );
